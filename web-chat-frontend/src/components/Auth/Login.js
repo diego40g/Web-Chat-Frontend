afa6347b57724a3f511fc74da316e8434a84251e
diego40g <diegopaznaula@hotmail.com>
2022-11-25 23:33:04 -0500

    the struct html of login view

--- web-chat-frontend/src/components/Auth/Login.js
+++ web-chat-frontend/src/components/Auth/Login.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import loginImage from '../../assets/images/login.svg'
 
 const Login = () => {
   return (
-    <>
-        Login
-        <p>123</p>
-    </>
+    <div id='auth-container'>
+        <div id='auth-card'>
+            <div>
+                <div id='image-section'>
+                    <img src={loginImage} alt='Login' />
+                </div>
+                <div id='form-section'>
+                    <h2>Welcome back</h2>
+
+                    <form>
+                        <div className='input-field'>
+                            <input placeholder='Email'/>
+                        </div>
+
+                        <div className='input-field'>
+                            <input placeholder='Password'/>
+                        </div>
+
+                        <button>LOGIN</button>
+                    </form>
+
+                    <p>Don't have an account? <a href='#'>Register</a> </p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
