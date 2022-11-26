3cd5f9ae87a0d5cc59ca445b8cdbc48051a2ca79
diego40g <diegopaznaula@hotmail.com>
2022-11-25 23:19:38 -0500

    add page not found for routes

--- web-chat-frontend/src/App.js
+++ web-chat-frontend/src/App.js
@@ -8,12 +8,13 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <h1>1</h1>
       <div className="App">
         <Routes>
           <Route path='/' element={<Chat/>} />
           <Route path='/login' element={ <Login /> } />
           <Route path='/register' element={<Register/>} />
+
+          <Route path='*' element={<h1>404 page not found</h1>} />
         </Routes>
       </div>
     </Router>
