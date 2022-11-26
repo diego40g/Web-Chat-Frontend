2a4ee221bf9eddd463633fdf441e82222698486e
diego40g <diegopaznaula@hotmail.com>
2022-11-25 23:17:01 -0500

    install react router dom and use Routes, router and route to mange components

--- web-chat-frontend/src/App.js
+++ web-chat-frontend/src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
-import Chat from "./components/Chat";
+import Chat from "./components/Chat/Chat";
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Chat />
-      <Login />
-      <Register />
-    </div>
+    <Router>
+      <h1>1</h1>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Chat/>} />
+          <Route path='/login' element={ <Login /> } />
+          <Route path='/register' element={<Register/>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
