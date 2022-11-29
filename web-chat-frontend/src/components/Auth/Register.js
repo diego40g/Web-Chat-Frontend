f6ebf592f01abdea7ec48b25f8ef8afcec1a9ba5
diego40g <diegopaznaula@hotmail.com>
2022-11-28 21:36:02 -0500

    struct html of register and redirect to login

--- web-chat-frontend/src/components/Auth/Register.js
+++ web-chat-frontend/src/components/Auth/Register.js
@@ -1,8 +1,55 @@
 import React from 'react'
+import registerImage from '../../assets/images/register.svg'
+
+import './Auth.scss'
 
 const Register = () => {
   return (
-    <h1>Register</h1>
+    <div id="auth-container">
+      <div id='auth-card'>
+        <div className='card-shadow'>
+          <div id='image-section'>
+              <img src={registerImage} alt='Register' />
+          </div>
+
+          <div id='form-section'>
+            <h2>Create an account</h2>
+            <form>
+              <div className='input-field mb-1'>
+                <input placeholder='First name' />
+              </div>
+
+              <div className='input-field mb-1'>
+                <input placeholder='Last name' />
+              </div>
+
+              <div className='input-field mb-1'>
+                <input placeholder='Email' />
+              </div>
+
+              <div className='input-field mb-1'>
+                <select>
+                  <option value='male'>Male</option>
+                  <option value='female'>Female</option>
+                  <option value='not'>I prefer not to say</option>
+                </select>
+              </div>
+
+              <div className='input-field mb-2'>
+                <input placeholder='Password' />
+              </div>
+              <div className='input-field mb-2'>
+                <input placeholder='Repeat password' />
+              </div>
+
+              <button>REGISTER</button>
+            </form>
+
+            <p>Already have an account? <a href='/login'>Login</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
