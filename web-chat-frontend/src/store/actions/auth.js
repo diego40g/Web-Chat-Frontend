f354d62b25f9c91d7c4200ce869f25845694ba7d
diego40g <diegopaznaula@hotmail.com>
2022-11-29 20:48:02 -0500

    install redux and redux thunk. Create auth action

---
+++ web-chat-frontend/src/store/actions/auth.js
@@ -0,0 +1,15 @@
+import AuthService from '../../services/authService'
+
+export const LOGIN = 'LOGIN'
+
+export const login = (params) => dispatch => {
+    return AuthService.login(params)
+    .then(data => {
+        console.log(data);
+        dispatch({ type: LOGIN, payload: data })
+    })
+    .catch(err => {
+        console.log(err);
+        throw err
+    })
+}
