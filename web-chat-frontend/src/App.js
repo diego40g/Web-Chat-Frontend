21357be65005e70482f381e257133ded63ca1500
diego40g <diegopaznaula@hotmail.com>
2022-11-30 23:08:04 -0500

    Redirect route of chat to login

--- web-chat-frontend/src/App.js
+++ web-chat-frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.scss';
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import Chat from "./components/Chat/Chat";
+import ProtectedRoute from "./components/Router/ProtectedRoute";
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -10,7 +11,9 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path='/' element={<Chat/>} />
+          <Route element={ <ProtectedRoute/> }>
+            <Route path='/' element={ <Chat /> } />
+          </Route> 
           <Route path='/login' element={ <Login /> } />
           <Route path='/register' element={<Register/>} />
 
