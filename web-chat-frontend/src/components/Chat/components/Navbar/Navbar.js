a1df26c83ac6e656429147999d08a150fb17f1c3
diego40g <diegopaznaula@hotmail.com>
2022-11-30 23:30:57 -0500

    create a navbar struct html and styles

---
+++ web-chat-frontend/src/components/Chat/components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import './Navbar.scss'
+
+const Navbar = () => {
+    const user = useSelector(state => state.authReducer.user)
+    return(
+        <div id="navbar" className="card-shadow">
+            <h2>dipaz - Chat</h2>
+            <div id="profile-menu">
+                <img src="" alt="Avatar"/>
+                <p></p>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
