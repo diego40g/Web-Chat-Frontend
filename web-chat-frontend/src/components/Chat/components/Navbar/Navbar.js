3af60220a70b3181da7bf471a98255856eae95dd
diego40g <diegopaznaula@hotmail.com>
2022-12-01 20:20:26 -0500

    profile options open, close and styles using event click

--- web-chat-frontend/src/components/Chat/components/Navbar/Navbar.js
+++ web-chat-frontend/src/components/Chat/components/Navbar/Navbar.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Navbar.scss'
 
 const Navbar = () => {
     const user = useSelector(state => state.authReducer.user)
+    const [showProfileOptions, setShowProfileOptions]=useState('')
+
     return(
         <div id="navbar" className="card-shadow">
             <h2>dipaz - Chat</h2>
-            <div id="profile-menu">
+            <div onClick={() => setShowProfileOptions(!showProfileOptions)} id="profile-menu">
                 <img src={user.avatar} alt="Avatar"/>
                 <p>{user.firstName} {user.lastName}</p>
                 <FontAwesomeIcon icon='caret-down' className="fa-icon"/>
+
+                {
+                    showProfileOptions &&
+                    <div id="profile-options">
+                        <p>Update profile</p>
+                        <p>Logout</p>
+                    </div>
+                }
             </div>
         </div>
     )
