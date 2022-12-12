5dc18c984780877ac68289e3592b52799eaac616
diego40g <diegopaznaula@hotmail.com>
2022-12-11 23:32:14 -0500

    add a useEffect and dispatch for  call the fetch chat function

--- web-chat-frontend/src/components/Chat/Chat.js
+++ web-chat-frontend/src/components/Chat/Chat.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React,{ useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import Navbar from './components/Navbar/Navbar'
+import { fetchChats } from '../../store/actions/chat'
 
 import './Chat.scss'
 const Chat = () => {
 
+  const dispatch = useDispatch()
   const user = useSelector(state => state.authReducer.user)
+
+  useEffect(() => {
+    dispatch(fetchChats()).then(res => console.log(res)).catch(err=>console.log(err))
+  }, [dispatch])
 
   return (
     <div id='chat-container'>
